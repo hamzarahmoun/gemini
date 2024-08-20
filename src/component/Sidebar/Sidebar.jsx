9f45c3066a2e3451e184ea043b2b2ac4284c21d9
hamzarahmoun <hamza.rahmoun456@gmail.com>
2024-08-20 13:43:54 +0100

    left-side

--- src/component/Sidebar/Sidebar.jsx
+++ src/component/Sidebar/Sidebar.jsx
@@ -7,10 +7,39 @@ const Sidebar = () => {
     <div className='sidebar'>
         <div className="top">
             <img src={assets.menu_icon} alt="" />
+            <div className='new-chat'>
+              <img src={assets.plus_icon} alt="" />
+              <p> New Chat</p>
+            </div>
+            <div className="recent">
+              <p className="recent-title">Recent</p>
+              <div className="recent-entry">
+                <img src={assets.message_icon} alt="" />
+                <p>What is React</p>
+              </div>
+            </div>
+            
         </div>
         <div className="bottom">
+          <div className="bottom-item recent-entry" >
+            <img src={assets.question_icon} alt=''/>
+            <p >Help</p>
+          </div>
+          <div className="bottom-item recent-entry" >
+            <img src={assets.history_icon} alt=''/>
+            <p >Activity</p>
+          </div>
+          <div className="bottom-item recent-entry" >
+            <img src={assets.setting_icon} alt=''/>
+            <p >Setting</p>
+          </div>
+
 
         </div>
+      <div>
+        
+      </div> 
+        
 
     </div>
   )
