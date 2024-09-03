de256d3b08a81a9fde33654ff7abb746e52faebe
hamzarahmoun <hamza.rahmoun456@gmail.com>
2024-09-03 15:40:20 +0100

    search box

--- src/component/Main/Main.jsx
+++ src/component/Main/Main.jsx
@@ -31,7 +31,22 @@ const Main = () => {
                 <img src={assets.code_icon} alt="" />
             </div>
             </div>
-            
+
+            <div className="main-bottom">
+                <div className="search-box">
+                    <input type="text" placeholder='enter a prompt here' />
+                    <div>
+                        <img src={assets.gallery_icon} alt="" />
+                        <img src={assets.mic_icon} alt="" />
+                        <img src={assets.send_icon} alt="" />
+
+                    </div>
+                </div>
+                <p className="bottom-info">
+                    Gemini may give inaccurate information, including misinformation and harmful content
+                </p>
+
+            </div>
         </div>
     </div>
   )
