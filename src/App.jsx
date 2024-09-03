c50588a0dac0b3b3c34b08b41893b9c65ed8702f
hamzarahmoun <hamza.rahmoun456@gmail.com>
2024-09-03 15:20:58 +0100

    main-container

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Sidebar from './component/Sidebar/Sidebar'
+import Main from './component/Main/Main'
+
 
 const App = () => {
   return (
     <>
-      <Sidebar/>    
+      <Sidebar/>   
+      <Main/>
     </>
   )
 }
---
+++ src/component/Main/Main.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { assets } from '../../assets/assets'
+import './Main.css'
+const Main = () => {
+  return (
+    <div className='main'>
+        <div className="nav">
+           <p>Gemini</p>
+           <img src={assets.user_icon} alt="" />
+        </div>
+        <div className="main-container">
+            <div className="greet">
+                <p><span>Hello, Dev.</span></p>
+                <p>How Can I Help You Today?</p>
+            </div>
+            <div className="cards">
+                <div className="card">
+                <p>Suggest beutiful place to see on an upcoming road trip</p>
+                <img src={assets.compass_icon} alt="" />
+            </div>
+            <div className="card">
+                <p>briefly explain the process to build an AI chatbot</p>
+                <img src={assets.bulb_icon} alt="" />
+            </div>
+            <div className="card">
+                <p>brainstorm a few topics for a new blog post</p>
+                <img src={assets.message_icon} alt="" />
+            </div>
+            <div className="card">
+                <p>improve the copy on my website</p>
+                <img src={assets.code_icon} alt="" />
+            </div>
+            </div>
+            
+        </div>
+    </div>
+  )
+}
+export default Main
